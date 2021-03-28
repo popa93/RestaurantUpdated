6ee5cc8a6fc75d9e0be6f3417999e76cd6266fa3
Popa Andrei <popaandrei2001@yahoo.com>
2021-03-28 15:18:22 +0300

    ADD:just a test23

--- app/src/main/java/view/SplashScreenFragment.java
+++ app/src/main/java/view/SplashScreenFragment.java
@@ -91,6 +91,8 @@ public class SplashScreenFragment extends Fragment {
 
     private void jumpToAdmin() {
         //just for test
+        //just for test2
+        //just for test3
         NavDirections action = SplashScreenFragmentDirections.actionSplashScreenFragmentToAdminMainFragment2();
         if (getView() != null) {
             Navigation.findNavController(getView()).navigate(action);
